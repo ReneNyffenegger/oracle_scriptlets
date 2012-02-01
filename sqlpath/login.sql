f88eb41bfe18809e92aa008cec572a4f3bf19091
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2012-02-01 19:17:55 +0100

    Added login.sql

---
+++ sqlpath/login.sql
@@ -0,0 +1,8 @@
+set serveroutput on size 999999 format wrapped
+set feedback off
+set lines            190
+set pages           5000
+set long          100000
+set longchunksize 100000
+
+define _editor=gvim
