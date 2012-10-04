00a8a3a1aec4755b8e31cbf14728c8bf5418691a
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2012-10-04 15:45:05 +0200

    better prompt

--- sqlpath/login.sql
+++ sqlpath/login.sql
@@ -9,3 +9,15 @@ set tab              off
 define _editor=gvim
 
 alter session set nls_date_format = 'dd.mm.yyyy hh24:mi:ss';
+
+--  SQL Prompt {
+set termout off
+define sqlprompt=none
+column sqlprompt new_value sqlprompt
+
+select lower(sys_context('USERENV','CURRENT_USER')) || '@' || sys_context('USERENV','DB_NAME') as sqlprompt from dual;
+
+set sqlprompt '&sqlprompt> '
+undefine sqlprompt
+set termout on
+-- }
