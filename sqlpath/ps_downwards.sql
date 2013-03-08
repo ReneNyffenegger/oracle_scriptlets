7373637bb394cc10abc1d03ba5905344dae5a966
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2013-03-08 10:22:05 +0100

    + Unifying ps_upwards / ps_downwards

--- sqlpath/ps_downwards.sql
+++ sqlpath/ps_downwards.sql
@@ -1,7 +1,1 @@
-define complete_name=&1
-column signature new_value signature
-select signature from plscope_callable where lower(complete_name) = lower('&complete_name');
-@spool c:\temp\ps_downwards.dot
-exec plscope.print_downwards_graph('&signature', 'dot');
-spool off
-@dot c:\temp\ps_downwards
+@ps_ &1 print_upwards_graph
--- sqlpath/ps_upwards.sql
+++ sqlpath/ps_upwards.sql
@@ -1,10 +1,1 @@
---
---     See ../plscope and ./ps_find_call_path.sql
---
-define complete_name=&1
-column signature new_value signature
-select signature from plscope_callable where lower(complete_name) = lower('&complete_name');
-@spool c:\temp\ps_upwards.dot
-exec plscope.print_upwards_graph('&signature', 'dot');
-spool off
-@dot c:\temp\ps_upwards
+@ps_ &1 print_upwards_graph
