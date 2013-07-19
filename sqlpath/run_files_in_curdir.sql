87df6289415121b580528ec50963d770951464eb
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2013-07-19 14:40:31 +0200

    wrong gaensefuesschen removed

--- sqlpath/run_files_in_curdir.sql
+++ sqlpath/run_files_in_curdir.sql
@@ -3,6 +3,6 @@
 --
 --   Executes («run») the files in the current directory
 --
-$echo "-- create new file or delete existing" > %temp%\run_files_in_curdir_.sql
+$echo -- create new file or delete existing > %temp%\run_files_in_curdir_.sql
 $@for /f "usebackq" %a in (`dir /b`) do @echo @ %a >> %temp%\run_files_in_curdir_.sql
 @%TEMP%/run_files_in_curdir_
