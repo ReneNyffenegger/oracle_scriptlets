bf8136bf76de9818e61b679adc59572ff854355e
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2013-08-05 15:12:59 +0200

    + dep.sql

---
+++ sqlpath/dep.sql
@@ -0,0 +1,27 @@
+--
+--   Find object dependencies, report them in a «hierarchical» layout.
+--
+with obj (name, owner, type, level_) as (
+   select 
+     upper('&object_name'),
+     user,
+     upper('&object_type'), 
+     0
+   from
+     dual
+ union all
+   select
+     dep.name,
+     dep.owner,
+     dep.type,
+     obj.level_ + 1
+   from
+     obj  join dba_dependencies
+     dep  on obj.name  = dep.referenced_name  and
+             obj.owner = dep.referenced_owner and
+             obj.type  = dep.referenced_type
+)
+select
+    lpad(' ', level_ * 2) || name || ' [' || owner || ']' || ' {' || type || '}'
+from
+  obj;
