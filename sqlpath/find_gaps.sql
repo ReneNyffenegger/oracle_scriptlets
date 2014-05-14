3d24ecf4de3a05d536cc348a2e0e8c2c5b39c862
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-05-14 11:15:16 +0200

    Added columnn "Size"

--- sqlpath/find_gaps.sql
+++ sqlpath/find_gaps.sql
@@ -5,13 +5,14 @@
 --     numeric, non fractional values (integers), possibly 
 --     created by sequences.
 --
-select a+1   "From/Start Value", 
-       lead_ "To (Value)"
+select a+1        "From/Start Value", 
+       lead_      "To (Value)",
+       lead_ - a  "Size"
   from (
 select &&column                                            a,
        lag (&&column) over (order by &&column) lag_,
        lead(&&column) over (order by &&column) lead_,
-       &&column - lead(&&column) over (order by &&column) count_
+      &&column - lead(&&column) over (order by &&column) count_
   from &&table
  order by &&column - lead(&&column) over (order by &&column)
 )
