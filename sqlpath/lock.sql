4a72033fae783389274c486eeb2bf8bfa618abf0
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-04 09:36:13 +0200

    Identify objects and rowid, too.

--- sqlpath/lock.sql
+++ sqlpath/lock.sql
@@ -2,25 +2,43 @@
 --   Who blocks whom
 --
 
-select
-  l_blocker.sid,
+with locks as (
+  select /*+ materialize */
+    l_blocker.sid       blocker_sid,
+    l_blocker.lmode     has_mode,
+    l_blocker.type      blocker_type,
+    l_blockee.sid       blockee_sid,
+    l_blockee.request   wants_mode,
+    l_blockee.type      blockee_type
+  from
+    v$lock         l_blocker,
+    v$lock         l_blockee
+  where 
+    l_blocker.block          = 1                   and  -- Identify blocking sessions
+    l_blockee.request        > 0                   and  -- Identify blocked sessions
+    l_blocker.id1            = l_blockee.id1       and 
+    l_blocker.id2            = l_blockee.id2         
+)
+select 
+  locks    .blocker_sid,
   s_blocker.osuser,
-  l_blocker.lmode     has_mode,
-  l_blocker.type,
-' blocks '            blocks,
-  l_blockee.sid,
+  locks    .has_mode,
+  locks    .blocker_type,
+ 'blocks'                  blocks,
+  locks    .blockee_sid,
   s_blockee.osuser,
-  l_blockee.request   wants_mode,
-  l_blockee.type
-from
-  v$lock     l_blocker,
-  v$lock     l_blockee,
-  v$session  s_blocker,
-  v$session  s_blockee
- where 
-   l_blocker.block   = 1  and 
-   l_blockee.request > 0  and 
-   l_blocker.id1     = l_blockee.id1 and 
-   l_blocker.id2     = l_blockee.id2 and
-   s_blocker.sid     = l_blocker.sid and
-   s_blockee.sid     = l_blocker.sid;
+  locks    .wants_mode,
+  locks    .blockee_type,
+ '|'                       " ",
+  obj      .object_name,
+  dbms_rowid.rowid_create(1, s_blockee.row_wait_obj#, s_blockee.row_wait_file#, s_blockee.row_wait_block#, s_blockee.row_wait_row#) "rowid"
+from 
+  locks,
+  v$session    s_blockee,
+  v$session    s_blocker,
+  dba_objects  obj
+where
+  locks.blockee_sid       = s_blockee.sid and
+  locks.blocker_sid       = s_blocker.sid and
+  s_blockee.row_wait_obj# = obj.object_id(+)
+;
