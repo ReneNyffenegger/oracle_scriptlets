3db807dcb14027ad1b9a986312210149d1ef3ddb
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-24 11:04:03 +0200

    Correct last commit

--- sqlpath/cons.sql
+++ sqlpath/cons.sql
@@ -5,12 +5,10 @@ declare
 -- Parameter: name of constraint
 --
 -- Currently: only PK, FK, unique and check constraints supported.
---- NOT NULL constraints not yet supported.
+---           NOT NULL constraints not yet supported.
 
-   r_constraint all_constraints%rowtype;
+   r_constraint    all_constraints%rowtype;
    r_constraint_pk all_constraints%rowtype;
-
-   r_index      all_indexes%rowtype;
  
 begin
 
@@ -43,45 +41,45 @@ begin
 
    end;
  
-   if r_constraint.constraint_type = 'R' then -- {
+   if    r_constraint.constraint_type =   'R'       then -- {
  
          select * into r_constraint_pk from all_constraints where constraint_name = r_constraint.r_constraint_name;
  
-         dbms_output.put_line(' Foreign Key constraint');
+         dbms_output.put_line('  Foreign Key constraint');
          dbms_output.new_line;
-         dbms_output.put_line(' ' || rpad(r_constraint.table_name, 30) || ' -> ' || r_constraint_pk.table_name);
-         dbms_output.put_line(' ' || rpad('-', 30, '-') || ' ' || rpad('-', 30, '-'));
+         dbms_output.put_line('    ' || rpad(r_constraint.table_name, 30) || ' -> ' || r_constraint_pk.table_name);
+         dbms_output.put_line('    ' || rpad('-', 30, '-')                || '    ' || rpad('-', 30, '-'));
  
          for cols in (
  
              select
-               fk_col.column_name column_name_fk,
-               pk_col.column_name column_name_pk
+               fk_col.column_name  column_name_fk,
+               pk_col.column_name  column_name_pk
              from
-               all_cons_columns fk_col,
-               all_cons_columns pk_col
+               all_cons_columns   fk_col,
+               all_cons_columns   pk_col
              where
-               fk_col.constraint_name = r_constraint .constraint_name and
+               fk_col.constraint_name = r_constraint   .constraint_name and
                pk_col.constraint_name = r_constraint_pk.constraint_name and
-               fk_col.position = pk_col.position
+               fk_col.position        = pk_col.position
              order by
                fk_col.position
                 
          ) loop
  
-           dbms_output.put_line(' ' || rpad(cols.column_name_fk, 30) || ' ' || cols.column_name_pk);
+           dbms_output.put_line('    ' || rpad(cols.column_name_fk, 30) || '    ' || cols.column_name_pk);
  
          end loop;
    -- }
    elsif r_constraint.constraint_type in ('P', 'U') then -- {
 
          if r_constraint.constraint_type = 'P' then
-            dbms_output.put_line(' Primary Key constraint');
+            dbms_output.put_line('  Primary Key constraint');
          else
-            dbms_output.put_line(' Unique key constraint');
+            dbms_output.put_line('  Unique key constraint');
          end if;
          dbms_output.new_line;
-         dbms_output.put_line(' Table: ' || r_constraint.table_name);
+         dbms_output.put_line('     Table: ' || r_constraint.table_name);
          dbms_output.new_line;
 
          for cols in (
@@ -93,23 +91,23 @@ begin
 
          ) loop
               
-           dbms_output.put_line(' ' || cols.column_name);
+           dbms_output.put_line('            ' || cols.column_name);
 
          end loop;
  
    -- }
-   elsif r_constraint.constraint_type = 'C' then -- {
+   elsif r_constraint.constraint_type =   'C'       then -- {
 
-         dbms_output.put_line(' Check constraint');
+         dbms_output.put_line('  Check constraint');
          dbms_output.new_line;
-         dbms_output.put_line(' Condition: ' || r_constraint.search_condition);
+         dbms_output.put_line('     Condition: ' || r_constraint.search_condition);
          dbms_output.new_line;
-         dbms_output.put_line(' Table: ' || r_constraint.table_name);
-         dbms_output.put (' Columns: ');
+         dbms_output.put_line('     Table:     ' || r_constraint.table_name);
+         dbms_output.put     ('     Columns:   ');
 
          for cols in (
 
-             select /* position, */ -- Seems to be null for check constraints...
+             select /* position, */  -- Seems to be null for check constraints...
                     row_number() over (order by position) row_,
                     column_name
                from all_cons_columns
@@ -121,7 +119,7 @@ begin
            if cols.row_ = 1 then
               dbms_output.put_line(cols.column_name);
            else
-              dbms_output.put_line(' ' || cols.column_name);
+              dbms_output.put_line('                ' || cols.column_name);
            end if;
 
          end loop;
