fc5e6a1638617beb728f084a4e453f539c5bfcd4
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-12-11 15:17:18 +0100

    Join with dba_objects etc.

--- sqlpath/find_unreferenced_objects.sql
+++ sqlpath/find_unreferenced_objects.sql
@@ -3,18 +3,26 @@
 --
 --  See also -> dep.sql and -> ref.sql
 --
+select owner, lower(object_name), object_name, object_type from (
   select
     owner,
     object_name
   from
-    dba_objects
-  where
-    owner        = '&1' and
-    object_type not in ('INDEX', 'INDEX PARTITION', 'TRIGGER', 'JOB')
+    dba_objects 
+  where 
+    owner = '&owner' and
+    object_type not in ('INDEX', 'INDEX PARTITION', 'INDEX SUBPARTITION', 'TABLE PARTITION', 'LOB', 'LOB PARTITION', 'TRIGGER', 'JOB', 'SYNONYM')
 minus
-  select
-    referenced_owner,
-    referenced_name
+select
+  referenced_owner, referenced_name 
   from
     dba_dependencies
-  where type not in ('SYNONYM');
+  where type not in ('SYNONYM')
+) join
+user_objects using (object_name)
+where 
+   object_type not in ('VIEW', 'TABLE') and
+   object_type != 'SEQUENCE' and
+   object_type = 'TYPE'
+order by object_name
+;
