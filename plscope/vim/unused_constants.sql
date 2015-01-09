96857800ad163593131408ff39eaab48fc107f35
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-01-09 08:42:53 +0100

    Added vim folder

---
+++ plscope/vim/unused_constants.sql
@@ -0,0 +1,28 @@
+--
+--    :set efm=%f-%l-%c-%m
+--    :cf unused_constants.ef
+--
+
+@spool unused_constants.ef
+
+select distinct
+  lower(x.object_name) || 
+    case x.object_type 
+         when 'PACKAGE BODY' then '.pkb' 
+         when 'PACKAGE'      then '.pks'
+    end || '-' ||
+  x.line                         || '-' ||
+  x.col                          || '-' ||
+  x.name   t
+from (
+   select signature from tq84_all_identifiers where usage in ('DECLARATION') and type in ('CONSTANT') and owner = user
+   minus
+   select signature from tq84_all_identifiers where usage in ('REFERENCE') and owner = user
+) o, tq84_all_identifiers x 
+where o.signature = x.signature
+order by 
+  1
+;
+
+spool off
+
