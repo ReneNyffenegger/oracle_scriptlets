5a7b226f659cebf1fd0af9281fb930b53a816c02
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-02-03 14:31:08 +0100

    Use all_objects instead of user_objects

--- sqlpath/drop_if_exists.sql
+++ sqlpath/drop_if_exists.sql
@@ -7,21 +7,27 @@ declare
 --       drop_schema_if_exists.sql
 --
 
-  obj_name varchar2(30) := '&1';
-  obj_type varchar2(30);
+  obj_name  varchar2(30) := '&1';
+  obj_owner varchar2(30);
+  obj_type  varchar2(30);
 
 begin
 
-  select object_type into obj_type
-    from user_objects
-   where object_name = upper(obj_name) and
-         object_type not like '% BODY';
+  select owner, object_type into obj_owner, obj_type
+    from (
+      select owner, object_type,
+             row_number() over (order by case when owner = user then 1 else 2 end) r
+        from all_objects
+       where object_name = upper(obj_name) and
+             object_type not like '% BODY'
+    )
+    where r = 1;
 
-  execute immediate 
+  execute immediate
     'drop ' || obj_type || ' ' || obj_name || case when obj_type = 'TABLE' then ' purge' end;
 
 exception
-   when no_data_found then 
+   when no_data_found then
         null;
 
    when others then
