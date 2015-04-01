7738b30f4f03a8d1393a153aea41b57be6d87863
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-04-01 07:34:56 +0200

    New version, same functionality. Can now cope with statements longer then 4K.

--- sqlpath/sqlid.sql
+++ sqlpath/sqlid.sql
@@ -3,6 +3,41 @@
 --
 set     verify off
 
-select  listagg(sql_text, '') within group (order by piece)
-from    v$sqltext_with_newlines
-where   sql_id = '&1';
+--  Old Version: Could not cope with statement longer the 4K...
+--
+--      select listagg(sql_text, '') within group (order by piece)
+--      from   v$sqltext_with_newlines
+--      where  sql_id = '&1';
+
+declare
+  l    varchar2(4000);
+  c    char(1);
+begin
+
+  for s in (select sql_text
+              from v$sqltext_with_newlines
+             where sql_id = '&1'
+             order by piece)     loop
+
+    for i in 1 .. length(s.sql_text) loop
+
+        c := substr(s.sql_text, i, 1);
+
+        if c = chr(10) then
+
+           dbms_output.put_line(l);
+           l := '';
+
+        else
+
+           l := l || c;
+
+        end if;
+
+    end loop;
+  end loop;
+
+  dbms_output.put_line(l);
+
+end;
+/
