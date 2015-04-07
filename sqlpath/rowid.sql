97a695f7a3785b54228bedc69d49612459670507
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-04-07 13:38:21 +0200

    print subobject also if exists

--- sqlpath/rowid.sql
+++ sqlpath/rowid.sql
@@ -16,6 +16,7 @@ declare
   obj_name      varchar2(30);
   obj_owner     varchar2(30);
   obj_type      varchar2(30);
+  obj_sub       varchar2(30);
 
 begin
 
@@ -30,14 +31,19 @@ begin
     row_number    => row_number
   );
 
-  select object_name, owner    , object_type
-    into obj_name   , obj_owner, obj_type
+  select object_name, owner    , object_type, subobject_name
+    into obj_name   , obj_owner, obj_type   , obj_sub
     from dba_objects
    where object_id = object_number;
 
   dbms_output.new_line;
   dbms_output.put_line('Rowid Type:   ' || case rowid_type when dbms_rowid.rowid_type_restricted then 'Restricted' when dbms_rowid.rowid_type_extended then 'Extended' else '?' end);
   dbms_output.put_line('Object:       ' || obj_owner || '.' || obj_name || ' (' || obj_type || ') / ' || object_number);
+  if obj_sub is not null then
+     dbms_output.put_line('  Sub obj:    ' || obj_sub);
+  end if;
+
+
   dbms_output.put_line('Rel. Fileno:  ' || relative_fno);
   dbms_output.put_line('Block No:     ' || block_number);
   dbms_output.put_line('Row No:       ' || row_number  );
