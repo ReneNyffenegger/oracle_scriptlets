cd05283b8036fdede187a52843b7c0c723ca4df6
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-11-19 13:38:11 +0100

    Use who_am_i to determine caller

--- operation_log/operation_log.pkb
+++ operation_log/operation_log.pkb
@@ -5,6 +5,7 @@ create or replace package body operation_log as
 
     v_is_exception varchar2(1) := 'N';
     v_back_trace   varchar2(4000);
+    v_caller       varchar2(4000) := who_am_i(1);
   begin
 
     if is_exception then
@@ -13,7 +14,7 @@ create or replace package body operation_log as
       v_back_trace := dbms_utility.format_error_backtrace;
     end if;
 
-    insert into operation_log_table values (operation_log_seq.nextval, sysdate, txt, v_is_exception, null, v_back_trace);
+    insert into operation_log_table values (operation_log_seq.nextval, sysdate, txt, v_caller, v_is_exception, null, v_back_trace);
 
     commit;
 
--- operation_log/operation_log_table.sql
+++ operation_log/operation_log_table.sql
@@ -1,9 +1,10 @@
--- drop table operation_log_table purge;
+drop table operation_log_table purge;
 
 create table operation_log_table (
   id               number,
   tm               date                 not null,
   txt              varchar2(4000)       not null,
+  caller           varchar2(4000)       not null,
   is_exception     varchar2(1)          not null check(is_exception in ('N', 'Y')),
   id_parent        number,
   error_backtrace  varchar2(4000),
@@ -11,5 +12,5 @@ create table operation_log_table (
   constraint operation_log_fk1 foreign key (id_parent) references operation_log_table
 );
 
--- drop   sequence operation_log_seq;
+drop   sequence operation_log_seq;
 create sequence operation_log_seq;
