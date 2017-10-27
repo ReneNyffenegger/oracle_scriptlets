3727f2b098fcc84548187b7d5d9660302f1f05a7
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-10-27 10:22:35 +0200

    Update run_files_in_curdir.sql

--- sqlpath/run_files_in_curdir.sql
+++ sqlpath/run_files_in_curdir.sql
@@ -4,7 +4,7 @@
 --   Executes («run») the files in the current directory
 --
 define file_pattern=&1
-$echo -- create new file or delete existing > %temp%\run_files_in_curdir_.sql
+$echo -- Created by %SQLPATH%\run_files_in_curdir.sql > %temp%\run_files_in_curdir_.sql
 $@for /f "usebackq" %a in (`dir /b &file_pattern`) do @echo @ %a >> %temp%\run_files_in_curdir_.sql
 prompt %temp%\run_files_in_curdir_.sql created, running it...
 @%TEMP%/run_files_in_curdir_.sql
