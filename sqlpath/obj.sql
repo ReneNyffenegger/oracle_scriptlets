336c6da4fe2c9b67cf11771e2bd794b883658618
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2018-02-03 22:43:32 +0100

    Use substrb

--- sqlpath/obj.sql
+++ sqlpath/obj.sql
@@ -1,6 +1,11 @@
 select
-  object_name, object_type, owner
+  substrb(object_name, 1, 30) object_name,
+  substrb(object_type, 1, 30) object_type,
+  substrb(owner      , 1, 30) owner
 from
   all_objects
 where
-  lower(object_name) like lower('%&1%');
+  lower(object_name) like lower('%&1%')
+order by
+  owner,
+  object_name;
