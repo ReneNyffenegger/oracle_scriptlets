797ec5933f2a0bca807a6d20bcad96c84ecc5a9c
René Nyffenegger <mail@renenyffenegger.ch>
2018-07-26 11:32:18 +0200

    exclude TABLE PARTITION

--- sqlpath/obj.sql
+++ sqlpath/obj.sql
@@ -1,11 +1,12 @@
 select
-  substrb(object_name, 1, 30) object_name,
-  substrb(object_type, 1, 30) object_type,
-  substrb(owner      , 1, 30) owner
+   substrb(object_name, 1, 30) object_name,
+   substrb(object_type, 1, 30) object_type,
+   substrb(owner      , 1, 30) owner
 from
-  all_objects
+   dba_objects
 where
-  lower(object_name) like lower('%&1%')
+   lower(object_name) like lower('%&1%') and
+   object_type not in ('TABLE PARTITION')
 order by
-  owner,
-  object_name;
+   owner,
+   object_name;
